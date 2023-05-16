336b455f4e044fa322206723a99362ee34fb33a0
Sgarra <sgarraclaudia0@gmai.com>
2023-05-16 16:37:37 +0200

    change interface

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ window.columnconfigure(1, weight=1)
 window.columnconfigure(2, weight=1)
 
 
-frame1 = tk.Frame(window, background="blue", heigh="100", width="200")
+frame1 = tk.Frame(window, background="#BFC0CB", heigh="100", width="200")
 frame2 = tk.Frame(window, background="yellow", heigh="100", width="200")
-frame3 = tk.Frame(window, background="red", heigh="100",width="200")
+frame3 = tk.Frame(window, background="#FFFFFF", heigh="100",width="200")
 frame4 = tk.Frame(window, background="purple", heigh="100", width="200")
 
 frame1.pack(fill=X)
@@ -56,19 +56,19 @@ frame4.pack(fill=BOTH, expand=True, side=LEFT)
 
 
 
-label_welcome = tk.Label(frame1, text="Welcome in your smart room", font=("Microsoft YaHei",16, BOLD))
-label_welcome.pack(pady=20, padx=350)
+label_welcome = tk.Label(frame1, text="  Welcome in your smart room  ", bg='#BFC0CB', fg='#161EA1', font=("Microsoft YaHei",16, BOLD))
+label_welcome.pack(pady=10, padx=350, ipadx=20, ipady=20)
 
-button_simulate = tk.Button(frame1, text="Simulate sensors", font=("Microsoft YaHei",16), command=simulate_sensors)
-button_simulate.pack(padx=10, pady=10)
+button_simulate = tk.Button(frame1, text="Simulate sensors", bg='#898FD9', font=("Microsoft YaHei",12, BOLD), command=simulate_sensors)
+button_simulate.pack(padx=10, pady=5)
 
-label_action = tk.Label(frame1, text="Select your action", font=("Microsoft YaHei",10))
+label_action = tk.Label(frame1, text="Select your action", bg="#BFC0CB", font=("Microsoft YaHei",10))
 label_action.pack(pady=20, padx=350)
 
 action_selected = tk.StringVar()
 action_combobox = ttk.Combobox(frame1, textvariable=action_selected)
 action_combobox["values"] = ["study", "movie", "sleep", "music", "clean"]
-action_combobox.pack(padx=5)
+action_combobox.pack(pady=5)
 action_combobox["state"] = "readonly"
 
 
