e931fefd46fbcb9e75905e4b4530f4bab834549e
andmon97 <andrea.montemurro97@gmail.com>
2023-05-17 10:34:10 +0200

    explanation light and noise and correction of the facts

--- Explanation.py
+++ Explanation.py
@@ -32,10 +32,32 @@ def getExplanation(prolog):
     text = text + "The action selected is "+ effectors['action'] + ".\n"
     preference = list(prolog.query("preferencesInstance("+effectors['action']+", temp, V, E)"))
     print(preference[0]['V'])
-    if sensors['outside_temperature'] == preference[0]['V']:
+    if int(sensors['outside_temperature']) == int(preference[0]['V']):
          text = text + "Since the inside temperature is equal to the desired temperature (" + sensors['outside_temperature'] + ' == ' + preference[0]['V'] + "), the expert system doesn not performs any changes.\n"
-    elif sensors['inside_temperature'] < preference[0]['V'] and sensors['outside_temperature'] > preference[0]['V']:
+    elif int(sensors['inside_temperature']) < int(preference[0]['V']) and int(sensors['outside_temperature']) > int(preference[0]['V']):
         text = text  
-   
+
+    # decode the light setting
+    preference = list(prolog.query("preferencesInstance("+effectors['action']+", light, V, E)"))
+    if int(sensors['outside_brightness']) >= int(preference[0]['V']):
+        text = text + "Since the outside light is greater than the desired light, the expert system has turned off the light and opened the windows.\n"
+    else:
+        text = text + "Since the outside light is less than the desired light, the expert system has turned on the selected light at the desired value of brightness ("+  str(preference[0]['V']) +" and closed the windows.\n"
+
+    
+    # decode the noise setting
+    preference = list(prolog.query("preferencesInstance("+effectors['action']+", noise, V, E)"))
+    preferenceTemp = list(prolog.query("preferencesInstance("+effectors['action']+", temp, V, E)"))
+    if int(sensors['outside_noise']) > preference[0]['V']:
+        text = text + "Since the outside noise is greater than the desired noise, the expert system "
+        if int(sensors['outside_temperature']) == int(preferenceTemp[0]['V']):
+            text = text + "has closed the windows.\n"
+        else:
+            if int(sensors['outside_temperature']) < int(preferenceTemp[0]['V']):
+                text = text + "has closed the windowsd and has turned on the air conditioning because the temperature outside is greater than the temperature inside.\n"
+            else:
+                text = text + "has closed the windows and has turned on the radiator because the temperature outside is less than the temperature inside.\n"
+    else:
+        text = text + "Since the outside noise is less than the desired noise, the expert system has opened the windows.\n"
     return text
     
